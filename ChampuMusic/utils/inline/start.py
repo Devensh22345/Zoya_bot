f69756910f681ffe2628d11e8b837d092681141e
Devensh22345 <kumardevensh0@gmail.com>
2025-02-19 00:25:43 +0530

    Update start.py

--- ChampuMusic/utils/inline/start.py
+++ ChampuMusic/utils/inline/start.py
@@ -69,10 +69,6 @@ def music_start_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [
-            InlineKeyboardButton(text="˹ᴀʙᴏᴜᴛ˼", callback_data="about"),
-            InlineKeyboardButton(text="˹sᴜᴘᴘᴏʀᴛ˼", callback_data="support"),
-        ],
-        [InlineKeyboardButton(text="۞ ғᴇᴀᴛᴜʀᴇs ۞", callback_data="feature")],
+        [InlineKeyboardButton(text="۞ ғᴇᴀᴛᴜʀᴇs ۞", callback_data="settings_back_helper")],
     ]
     return buttons
